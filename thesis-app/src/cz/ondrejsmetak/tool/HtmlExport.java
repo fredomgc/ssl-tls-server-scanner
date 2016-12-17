52e460d642349b1fefa7d88b01a7e49733c28606
Ondrej Smetak <posta@ondrejsmetak.cz>
2016-12-17 16:10:25 +0100

    komentovani

--- thesis-app/src/cz/ondrejsmetak/tool/HtmlExport.java
+++ thesis-app/src/cz/ondrejsmetak/tool/HtmlExport.java
@@ -1,6 +1,5 @@
 package cz.ondrejsmetak.tool;
 
-import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
 import cz.ondrejsmetak.ResourceManager;
 import cz.ondrejsmetak.entity.Report;
 import cz.ondrejsmetak.entity.ReportMessage;
@@ -14,25 +13,43 @@ import java.io.Writer;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
+ * Creates HTML file, that contains results of completed scans
  *
  * @author Ondřej Směták <posta@ondrejsmetak.cz>
  */
 public class HtmlExport {
 
+	/**
+	 * Specifies a empty place in HTML template, that will be replaced with
+	 * content
+	 */
 	private static final String CONTENT_HOOK = "#CONTENT#";
 
+	/**
+	 * Returns prepared HTML template with no content.
+	 *
+	 * @return HTML template
+	 * @throws FileNotFoundException
+	 */
 	private String getTemplate() throws FileNotFoundException {
 		return Helper.getContentoOfFile(ResourceManager.getHtmlTemplate());
 	}
 
+	/**
+	 * Creates content of HTML export. This content may contains multiple scan
+	 * reports
+	 *
+	 * @param reports collection of completed scan reports
+	 * @param timestamp date and time of finished scans
+	 * @return body of HTML file
+	 * @throws FileNotFoundException
+	 */
 	private String getContent(List<Report> reports, Date timestamp) throws FileNotFoundException {
 		StringBuilder sb = new StringBuilder();
 		sb.append(String.format("<h1>SSL/TLS scan report <small>created %s</small></h1>", Helper.getFormattedDateTime(timestamp, false)));
-		
+
 		for (Report report : reports) {
 			sb.append(doReport(report));
 			sb.append("<hr />");
@@ -40,11 +57,15 @@ public class HtmlExport {
 
 		String template = getTemplate();
 		template = template.replace(CONTENT_HOOK, sb.toString());
-		//todo, zmenit cas vygenerovani
-		
 		return template;
 	}
 
+	/**
+	 * Create HTML table of one report
+	 *
+	 * @param report report of completed scan
+	 * @return HTML table
+	 */
 	private String doReport(Report report) {
 		List<ReportMessage> protocol = new ArrayList<>();
 		List<ReportMessage> certificate = new ArrayList<>();
@@ -68,7 +89,7 @@ public class HtmlExport {
 				cipher.add(reportMessage);
 			}
 		}
-		
+
 		StringBuilder sb = new StringBuilder();
 		sb.append(String.format("<h2>Target: %s (%s)</h2>", report.getTarget().getDestination(), report.getTarget().getName()));
 		sb.append("<table class=\"table table-striped table-hover\">");
@@ -83,6 +104,14 @@ public class HtmlExport {
 		return sb.toString();
 	}
 
+	/**
+	 * Create one table segment. Each segment represents one category of report
+	 * messages
+	 *
+	 * @param segmentName name of segment
+	 * @param messages collection containg report messages
+	 * @return HTML of table segment
+	 */
 	private String doCreateTableSegment(String segmentName, List<ReportMessage> messages) {
 		if (messages.isEmpty()) {
 			return "";
@@ -106,11 +135,17 @@ public class HtmlExport {
 		return sb.toString();
 	}
 
+	/**
+	 * According to type of report message, return valid CSS class
+	 *
+	 * @param message report message
+	 * @return css class
+	 */
 	private String typeToCssClass(ReportMessage message) {
-		if(message.getRequiredMode().isCanBe()){
+		if (message.getRequiredMode().isCanBe()) {
 			return "";
 		}
-		
+
 		if (message.getType().equals(ReportMessage.Type.ERROR)) {
 			return "danger";
 		}
@@ -122,27 +157,21 @@ public class HtmlExport {
 		return "";
 	}
 
-	public String export(List<Report> reports) {
+	/**
+	 * Exports collection of report messages to HTML file
+	 *
+	 * @param reports collection of report messages
+	 * @return path to newly created HTMl file
+	 * @throws IOException in case of any error
+	 */
+	public String export(List<Report> reports) throws IOException {
 		Date timestamp = new Date();
 		File target = new File(Helper.getWorkingDirectory() + File.separator + "report_" + Helper.getFormattedDateTime(timestamp, true) + ".htm");
 
-		Writer writer = null;
-
-		//todo, try with resources
-		try {
-
-			writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target.getAbsolutePath()), "utf-8"));
+		try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(target.getAbsolutePath()), "utf-8"))) {
 			writer.write(getContent(reports, timestamp));
 			writer.flush();
 			writer.close();
-
-		} catch (IOException ex) {
-			// report
-		} finally {
-			try {
-				writer.close();
-			} catch (Exception ex) {/*ignore*/
-			}
 		}
 
 		return target.getAbsolutePath();
